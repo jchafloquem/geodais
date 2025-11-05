15a05bd2a8cf58c06061fb43738791f624b0aeaa
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2025-11-05 10:10:45 -0500

    .

--- src/app/geovisor/geovisor.routes.ts
+++ src/app/geovisor/geovisor.routes.ts
@@ -5,31 +5,31 @@ export default [
 			{
 				path: 'map',
         title:'GeoCultivos DAIS',
-        //canActivate: [privateGuard()],
+        canActivate: [publicGuard()],
 				loadComponent: () => import('./pages/map/map.component').then(m => m.MapComponent),
 			},
       {
 				path: 'dashboard',
         title:'Dashboard',
-        //canActivate: [privateGuard()],
+        canActivate: [publicGuard()],
 				loadComponent: () => import('./pages/dashboard/dashboard.component').then ( m => m.DashboardComponent),
 			},
       {
 				path: 'reportes',
         title:'Reportes',
-        //canActivate: [privateGuard()],
+        canActivate: [publicGuard()],
 				loadComponent: () => import('./pages/reporte/report.component').then ( m => m.ReportComponent),
 			},
 			{
 				path: 'repositorio',
         title:'Repositorio',
-        //canActivate: [privateGuard()],
+        canActivate: [publicGuard()],
 				loadComponent: () => import('./pages/repositorio/repositorio.component'),
 			},
 			{
 				path: 'metadata',
         title:'Metadata',
-        //canActivate: [privateGuard()],
+        canActivate: [publicGuard()],
 				loadComponent: () => import('./pages/metadata/metadata.component'),
 			},
 			{
