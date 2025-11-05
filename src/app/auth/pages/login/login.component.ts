bb616eb3c7d1433c2a35b140feb13af70d25106e
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2025-11-05 11:28:28 -0500

    Mejora de iphone

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -13,7 +13,7 @@ import { CommonModule } from '@angular/common';
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
-export default class LoginComponent {
+export default class LoginComponent implements OnInit, OnDestroy {
 	private _formBuilder = inject(FormBuilder);
 	private _router = inject(Router);
 	private _http = inject(HttpClient);
@@ -26,6 +26,51 @@ export default class LoginComponent {
 		usuario: this._formBuilder.nonNullable.control('', [Validators.required]),
 		password: this._formBuilder.nonNullable.control('', [Validators.required])
 	});
+
+  private readonly backgroundImages = [
+    'assets/images/wallpapers/wallpaper1.png',
+    'assets/images/wallpapers/wallpaper2.png',
+    'assets/images/wallpapers/wallpaper3.png',
+    'assets/images/wallpapers/wallpaper2.png' // Se repite para el efecto de ida y vuelta
+  ];
+
+  public backgrounds = [
+    { url: '', fade: false },
+    { url: '', fade: false }
+  ];
+
+  private intervalId: any;
+  private imageIndex = 0;
+  private activeBgIndex = 0;
+
+  ngOnInit(): void {
+    this.startImageCarousel();
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  private startImageCarousel(): void {
+    this.backgrounds[0].url = `url('${this.backgroundImages[this.imageIndex]}')`;
+    this.backgrounds[1].url = '';
+    this.backgrounds[0].fade = false; // Visible
+    this.backgrounds[1].fade = true;  // Oculto
+    this.activeBgIndex = 0;
+
+    const intervalDuration = 8750; // 8.75 segundos por imagen
+
+    this.intervalId = setInterval(() => {
+      this.imageIndex = (this.imageIndex + 1) % this.backgroundImages.length;
+      const nextBgIndex = (this.activeBgIndex + 1) % 2;
+      this.backgrounds[nextBgIndex].url = `url('${this.backgroundImages[this.imageIndex]}')`;
+      this.backgrounds[this.activeBgIndex].fade = true;
+      this.backgrounds[nextBgIndex].fade = false;
+      this.activeBgIndex = nextBgIndex;
+    }, intervalDuration);
+  }
 
   async submit(): Promise<void> {
     if (this.form.invalid) {
